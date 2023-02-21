57b55da3e04089698c56592bf27486ac1fd9ad46
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-02-21 17:19:18 +0100

    lab01zad01

---
+++ lab01/lab01zad01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("How old are you?")
+	var i int
+	_, err := fmt.Scanf("%d", &i)
+	
+    if err != nil {
+		fmt.Println("error")
+	}
+
+	fmt.Printf("You have lived %d months and %d days!\n", i * 12, i * 12 * 365)
+}
