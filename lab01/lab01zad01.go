2b0f48e3096b2ee10c414b7e214fe157f2debc39
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-02-23 17:04:35 +0100

    Added gitignore and readme

--- lab01/lab01zad01.go
+++ lab01/lab01zad01.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
-
 func main() {
 	fmt.Println("How old are you?")
-	
+
 	var i int
 	_, err := fmt.Scanf("%d", &i)
-	
-    if err != nil {
+
+	if err != nil {
 		fmt.Println("Error - wrong age!")
 		return
 	}
 
-	fmt.Printf("You have lived %d months and %d days!\n", i * 12, i * 12 * 365)
+	fmt.Printf("You have lived %d months and %d days!\n", i*12, i*12*365)
 }
--- lab01/lab01zad02.go
+++ lab01/lab01zad02.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("How old are you?")
 
-	var i int;
+	var i int
 	_, err := fmt.Scanf("%d", &i)
 
 	if err != nil {
@@ -14,11 +14,11 @@ func main() {
 	}
 
 	fmt.Println("Your age [in years] on different planets:")
-	fmt.Printf("Mercury: %10f\n", float32(i) * 0.24)
-	fmt.Printf("Venus: %10f\n", float32(i) * 0.62)
-	fmt.Printf("Mars: %10f\n", float32(i) * 1.88)
-	fmt.Printf("Jupiter: %10f\n", float32(i) * 11.87)
-	fmt.Printf("Saturn: %10f\n", float32(i) * 29.48)
-	fmt.Printf("Uranus: %10f\n", float32(i) * 84.07)
-	fmt.Printf("Neptun: %10f\n", float32(i) * 164.9)
+	fmt.Printf("Mercury: %10f\n", float32(i)*0.24)
+	fmt.Printf("Venus: %10f\n", float32(i)*0.62)
+	fmt.Printf("Mars: %10f\n", float32(i)*1.88)
+	fmt.Printf("Jupiter: %10f\n", float32(i)*11.87)
+	fmt.Printf("Saturn: %10f\n", float32(i)*29.48)
+	fmt.Printf("Uranus: %10f\n", float32(i)*84.07)
+	fmt.Printf("Neptun: %10f\n", float32(i)*164.9)
 }
