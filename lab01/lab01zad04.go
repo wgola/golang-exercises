967bab58fad1db25eef03d4097899e71fbac8c03
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-02-23 17:51:08 +0100

    lab01zad04

---
+++ lab01/lab01zad04.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	answer := rand.Intn(100) + 1
+
+	fmt.Println("Guess the number (1-100) or type 0 to exit: ")
+
+	var guess int
+	fmt.Scanf("%d", &guess)
+
+	var end bool
+
+	for !end {
+		if guess == 0 {
+			fmt.Println("See you later!")
+			end = true
+		} else if guess == answer {
+			fmt.Println("Congrats! You guessed the answer!")
+			end = true
+		} else {
+			if guess < answer {
+				fmt.Println("Your guess is too little!")
+			} else {
+				fmt.Println("Your guess is too big!")
+			}
+			fmt.Println("Try again or type 0 to exit")
+			fmt.Scanf("%d", &guess)
+		}
+	}
+}
