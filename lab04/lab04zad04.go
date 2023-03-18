034fa75dbbc49f3a615d0d5561410030c7393897
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-03-18 11:10:22 +0100

    lab04zad04

--- lab04/lab04zad04.go
+++ lab04/lab04zad04.go
@@ -3,10 +3,7 @@ package lab04
 import "fmt"
 
 func zad04() {
-	var (
-		matrix1 = [][]int{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}}
-		matrix2 = [][]int{{8, 7, 6}, {5, 4, 3}, {2, 1, 0}}
-	)
+	matrix1, matrix2 := generateMatrix(10), generateMatrix(10)
 
 	sum := addMatrix(matrix1, matrix2)
 	fmt.Println("Matrix 1: ")
@@ -15,6 +12,17 @@ func zad04() {
 	printMatrix(matrix2)
 	fmt.Println("Sum of matrixes: ")
 	printMatrix(sum)
+}
+
+func generateMatrix(size int) (matrix [][]int) {
+	for i := 0; i < size; i++ {
+		row := []int{}
+		for j := 0; j < size; j++ {
+			row = append(row, j+i*size)
+		}
+		matrix = append(matrix, row)
+	}
+	return
 }
 
 func addMatrix(matrix1, matrix2 [][]int) (sumMatrix [][]int) {
@@ -31,7 +39,7 @@ func addMatrix(matrix1, matrix2 [][]int) (sumMatrix [][]int) {
 func printMatrix(matrix [][]int) {
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
-			fmt.Printf("%v ", matrix[i][j])
+			fmt.Printf("%3d ", matrix[i][j])
 		}
 		fmt.Println()
 	}
